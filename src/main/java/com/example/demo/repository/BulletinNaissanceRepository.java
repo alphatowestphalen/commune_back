a7022d381eb89c9c8a6a589064135e88fbafcbb1
Tojonantenaina8 <tojonantenaina08@gmail.com>
2023-03-06 19:29:37 +0100

    fix: bulletin Naiss Repo

--- src/main/java/com/example/demo/repository/BulletinNaissanceRepository.java
+++ src/main/java/com/example/demo/repository/BulletinNaissanceRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.BulletinNaissance;
 
 public interface BulletinNaissanceRepository extends JpaRepository<BulletinNaissance, Long>{
+	
  Page<BulletinNaissance> findAll(Pageable pageable);
 
 }
