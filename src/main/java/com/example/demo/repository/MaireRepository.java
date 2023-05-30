805ea2850745bf4ebd566a732110e034b4faf9f7
Osmin <osm1.zandry@gmail.com>
2023-05-31 00:14:51 +0200

    feat: add restore

--- src/main/java/com/example/demo/repository/MaireRepository.java
+++ src/main/java/com/example/demo/repository/MaireRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Maire;
 @Repository
 public interface MaireRepository extends JpaRepository<Maire, Long> {
 
+	@Modifying
+    @Query(value ="UPDATE maire SET deleted = false WHERE id_maire = :idMaire", nativeQuery = true)
+    void restoreMaire(@Param("idMaire") Long idMaire);
 }
